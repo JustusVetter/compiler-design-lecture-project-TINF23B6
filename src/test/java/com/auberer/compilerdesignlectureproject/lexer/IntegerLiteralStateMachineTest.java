2b43dcc3434506af11588a5a94a5841440db18a2
Justus <vetter.justus.a23@dh-karlsruhe.de>
2025-04-16 15:07:56 +0200

    test: advance tests
    add further testcases

--- src/test/java/com/auberer/compilerdesignlectureproject/lexer/IntegerLiteralStateMachineTest.java
+++ src/test/java/com/auberer/compilerdesignlectureproject/lexer/IntegerLiteralStateMachineTest.java
@@ -20,4 +20,30 @@ public class IntegerLiteralStateMachineTest {
         }
         assertTrue(stateMachine.isInAcceptState());
     }
+
+    @Test
+    @DisplayName("Correct multidigit Integer")
+    public void testCorrectMultiDigitNumber() {
+        String input = "42019840666";
+        IntegerLiteralStateMachine stateMachine = new IntegerLiteralStateMachine();
+        stateMachine.init();
+        stateMachine.reset();
+        for (char c : input.toCharArray()) {
+            assertDoesNotThrow(() -> stateMachine.processInput(c));
+        }
+        assertTrue(stateMachine.isInAcceptState());
+    }
+
+    @Test
+    @DisplayName("incorrect Multi Digit Integer")
+    public void testIncorrectMultiDigitInt() {
+        String input = "00";
+        IntegerLiteralStateMachine stateMachine = new IntegerLiteralStateMachine();
+        stateMachine.init();
+        stateMachine.reset();
+        for (char c : input.toCharArray()) {
+            assertDoesNotThrow(() -> stateMachine.processInput(c));
+        }
+        assertFalse(stateMachine.isInAcceptState());
+    }
 }
